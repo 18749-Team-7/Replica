e5d83beaa8e431d714130ee6f6594d1eb7b7b2bc
jameszha <jameszha@andrew.cmu.edu>
2019-11-13 19:54:20 -0500

    added replica-to-replica code

--- replica.py
+++ replica.py
@@ -23,12 +23,16 @@ class Replica():
     Main Man
     """
 
-    def __init__(self, port, verbose):
-        self.port = port
+    def __init__(self, verbose):
         self.set_host_ip()
         self.ip = self.host_ip
         self.verbose = verbose
-        self.HB_port = 10000
+        self.client_port  = 5000
+        self.HB_port      = 10000
+        self.RM_port      = 15000
+        self.replica_port = 20000
+
+        self.good_to_go = False # Set to True once we are up to date with other replicas (via checkpointing + logging)
 
         # Global variables
         self.users = dict()
@@ -37,11 +41,23 @@ class Replica():
         self.msg_count = 0
         self.count_mutex = threading.Lock() # Lock on message_count
 
+        self.members = dict()
+        self.members_mutex = threading.Lock() # Lock on replica members dict
+
         # Start the heartbeat thread
         self.start_heartbeat(interval=1) # TODO: Don't hardcode these values. Interval = 1 sec
 
-        print(RED + "Starting chat server on " + str(self.host_ip) + ":" + str(self.port) + RESET)
-        self.chat_server()
+        # Start the RM thread
+        # Upon startup, this Replica will receive the add_replicas packet with its own IP. 
+        # It will initiate get_connection_from_old_replicas() to get up to date with the other replicas
+        threading.Thread(target=self.rm_thread,args=()).start()
+
+
+        if (self.good_to_go):
+            # Start the chat server
+            threading.Thread(target=self.print_membership_thread,args=(s, 1)).start()
+            print(RED + "Starting chat server on " + str(self.host_ip) + ":" + str(self.client_port) + RESET)
+            self.chat_server()
 
 
     def set_host_ip(self):
@@ -49,7 +65,9 @@ class Replica():
         s.connect(("8.8.8.8", 80))
         self.host_ip = s.getsockname()[0]
 
-    
+    ###############################################
+    # Heartbeat functions
+    ###############################################
     def heartbeat_thread(self, s, interval):
         while(True):
             try:
@@ -69,7 +87,7 @@ class Replica():
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCPIP
             s.connect((self.ip, self.HB_port))
-            print(RED + "Connected to local fault detector at: " + self.ip + ":" + str(self.port) + RESET)
+            print(RED + "Connected to local fault detector at: " + self.ip + ":" + str(self.HB_port) + RESET)
 
         except Exception as e:
             print(e)
@@ -77,10 +95,137 @@ class Replica():
 
         threading.Thread(target=self.heartbeat_thread,args=(s, interval)).start()
 
-    
 
+    ###############################################
+    # Replica Membership functions
+    ###############################################
+    def print_membership_thread(self, interval):
+        while True:
+            print("Current Membership Set:" + str(self.members))
+            time.sleep(interval)
+
+    def rm_thread(self):
+        # Use port 15000 for RM
+        try:
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # IPv4, TCPIP
+            s.bind((self.ip, self.RM_port))
+            s.listen()
+            conn, addr = s.accept()
+        except Exception as e:
+            print(e)
+
+        while True:
+            try:
+                data = s.recv(BUF_SIZE)
+                data = data.decode("utf-8")
+                data = json.loads(data)
+
+                if (data["type"] == "add_replicas"):
+                    self.members_mutex.acquire()
+                    for replica_ip in data["ip_list"]:
+                        if replica_ip in members:
+                            print(RED + "Received add_replicas ip (" + replica_ip + ") that was already in membership set" + RESET)
+                        else: 
+                            self.members[replica_ip] = None
+                    self.members_mutex.release()
+
+                    if self.ip in data["ip_list"]: # First connected as new member, need to get state from other members
+                        self.members_mutex.acquire()
+                        del members[self.ip]
+                        self.members_mutex.release()
+                        print(RED + "Saw own IP. Getting checkpoint from other replicas" + RESET) 
+                        self.get_connection_from_old_replicas()
+                    else: # Already member, need to connect to new members
+                        self.connect_to_new_replicas()
+
+                elif (data["type"] == "del_replicas"):
+                    self.members_mutex.acquire()
+                    for replica_ip in data["ip_list"]:
+                        if replica_ip not in members:
+                            print(RED + "Received del_replicas ip that was not in membership set" + RESET)
+                        else:
+                            self.members[replica_ip].close() # close the socket to the failed replica
+                            del self.members[replica_ip]
+                    self.members_mutex.release()
+
+                else:
+                    print(RED + "Received bad packet type from RM" + RESET)
+
+
+            except KeyboardInterrupt:
+                s.close()
+                return
+
+            except Exception as e:
+                print(e)
+
+    def get_connection_from_old_replicas(self):
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # IPv4, TCPIP
+        s.bind((self.ip, self.replica_port))
+        s.listen(5)
+        self.members_mutex.acquire()
+        try:
+            while(s_replica == None for _,s_replica in members.items()):
+                # Accept a new connection
+                conn, addr = s.accept()
+                members[addr] = conn
+                print(RED + "Received connection from existing replica at" + addr + RESET)
+                threading.Thread(target=self.replica_send_thread(),args=(conn,)).start()
+                threading.Thread(target=self.replica_receive_thread(),args=(conn,)).start()
+        except Exception as e:
+            print(e)
+        self.members_mutex.release()
+        self.good_to_go = True
+
+    def connect_to_new_replicas(self):
+        for addr in members:
+            if members[addr] == None:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCPIP
+                s.connect((addr, self.replica_port))
+                self.members_mutex.acquire()
+                members[addr] = s
+                self.members_mutex.release()
+                print(RED + "Connected to new replica at: " + self.ip + ":" + str(self.replica_port) + RESET)
+                threading.Thread(target=self.replica_send_thread(),args=(s,)).start()
+                threading.Thread(target=self.replica_receive_thread(),args=(s,)).start()
+
+            except Exception as e:
+                print(e)
+
+    def replica_send_thread(self, s):
+        replica_to_replica_count = 0
+        while True:
+            try:
+                data = "Ping from " + self.ip + " | " + str(replica_to_replica_count)
+                s.send(data.encode("utf-8"))
+                replica_to_replica_count = replica_to_replica_count + 1
+
+            except KeyboardInterrupt:
+                s.close()
+                return
+            except Exception as e:
+                print(e)
+                return
+
+    def replica_receive_thread(self, s):
+        while True:
+            try:
+                data = s.recv(BUF_SIZE)
+                data = data.decode("utf-8")
+                print(data)
+            except KeyboardInterrupt:
+                s.close()
+                return
+            except Exception as e:
+                print(e)
+                return
+    
+    ###############################################
+    # Chat client functions
+    ###############################################
     def broadcast(self, message):
-        # Inform all other clients that a new client has joined
+        # send message to all clients
         self.users_mutex.acquire()
         for _, s_client in self.users.items():
             s_client.send(message.encode("utf-8"))
@@ -104,6 +249,7 @@ class Replica():
             message["text"] = "Malformed packet"
             message = json.dumps(message)
             s.send(message.encode("utf-8"))
+            s.close()
             return 
 
         if (login_data["username"] in self.users): # Username already in use
@@ -196,14 +342,14 @@ class Replica():
 
     def chat_server(self):
         # Open listening socket of Replica
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # IPv4, TCPIP
-        self.s.bind((self.ip, self.port))
-        self.s.listen(5)
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # IPv4, TCPIP
+        s.bind((self.ip, self.client_port))
+        s.listen(5)
 
         try:
             while(True):
                 # Accept a new connection
-                conn, addr = self.s.accept()
+                conn, addr = s.accept()
                 print("Accepted new client")
                 # Initiate a client listening thread
                 threading.Thread(target=self.client_service_thread, args=(conn, addr)).start()
@@ -213,8 +359,8 @@ class Replica():
             for _, s_client in self.users.items():
                 s_client.close()
             self.users_mutex.release()
-            self.s.close()
-            print(RED + "Closing chat server on " + str(self.ip) + ":" + str(self.port) + RESET)
+            s.close()
+            print(RED + "Closing chat server on " + str(self.ip) + ":" + str(self.client_port) + RESET)
         except Exception as e:
             print(e)
 
@@ -222,7 +368,6 @@ class Replica():
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-p', '--port', help="Server port", type=int, default=5000)
     parser.add_argument('-v', '--verbose', help="Print every chat message", action='store_true')
 
     args = parser.parse_args()
@@ -232,7 +377,7 @@ if __name__ == '__main__':
     start_time = time.time()
 
     args = get_args()
-    replica_obj = Replica(args.port, args.verbose)
+    replica_obj = Replica(args.verbose)
 
     print("\nTotal time taken: " + str(time.time() - start_time) + " seconds")
 
